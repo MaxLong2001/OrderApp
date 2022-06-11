779fd6e7951bb1d0ab34d70cd820510f0d538707
MaxLong2001 <20373785@buaa.edu.cn>
2022-06-11 19:01:57 +0800

    lyz: 定义Dish类

--- src/backend/Dish.java
+++ src/backend/Dish.java
@@ -1,5 +1,67 @@
 package backend;
 
 public class Dish {
-    private
+    private String name;
+    private String introduction;
+    private double price;
+    private int sales;
+    private int remaining;
+    private int category;
+    private String picture;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getSales() {
+        return sales;
+    }
+
+    public void setSales(int sales) {
+        this.sales = sales;
+    }
+
+    public int getRemaining() {
+        return remaining;
+    }
+
+    public void setRemaining(int remaining) {
+        this.remaining = remaining;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
 }

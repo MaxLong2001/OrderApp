c222c7a79cd34375239cae126e0ef35a73cd92b3
MaxLong2001 <20373785@buaa.edu.cn>
2022-06-11 14:18:12 +0800

    lyz: 初始化本地数据库

--- src/database/Database.java
+++ src/database/Database.java
@@ -8,6 +8,9 @@ public class Database {
 
         /**
          * 本地数据库连接信息
+         * 根据本地情况修改
+         *
+         * 或可考虑在服务器上搭建数据库
          */
         final String url = "jdbc:mysql://localhost:3306/order";
         final String user = "root";

af6541311ea16127380ee6ec5124c43783b630b4
MaxLong2001 <20373785@buaa.edu.cn>
2022-06-11 18:50:31 +0800

    lyz: 数据可接口定义

--- src/database/Database.java
+++ src/database/Database.java
@@ -1,12 +1,16 @@
 package database;
 
+import backend.Dish;
+import backend.Order;
+
 import java.sql.*;
+import java.util.List;
 
 public class Database {
     /**
      * 本地数据库连接信息
      * 根据本地情况修改
-     *
+     * <p>
      * 或可考虑在服务器上搭建数据库
      */
     static final String URL = "jdbc:mysql://rm-bp10c5dfstb3h6q96mo.mysql.rds.aliyuncs.com/order_app";
@@ -21,6 +25,138 @@ public class Database {
         System.out.println("Connecting to database...");
         conn = DriverManager.getConnection(URL, USER, PASS);
         System.out.println("Connected to database.");
+    }
+
+    /**
+     * 根据用户名查询钱包余额
+     *
+     * @param userName 用户名
+     * @return 钱包余额
+     * @throws SQLException 数据库查询错误
+     */
+    public static double getWallet(String userName) throws SQLException {
+        return 0;
+    }
+
+    /**
+     * 查询用户订单列表
+     *
+     * @param userName 用户名
+     * @return 订单列表
+     * @throws SQLException 数据库查询错误
+     */
+    public static List<Order> getUserOrderList(String userName) throws SQLException {
+        return null;
+    }
+
+    /**
+     * 添加或修改订单
+     *
+     * @param order 订单
+     * @throws SQLException 数据库查询错误
+     */
+    public static void addOrder(Order order) throws SQLException {
+
+    }
+
+    /**
+     * 删除订单
+     *
+     * @param order 订单
+     * @throws SQLException 数据库查询错误
+     */
+    public static void deleteOrder(Order order) throws SQLException {
+
+    }
+
+    /**
+     * 更新钱包余额
+     *
+     * @param userName 用户名
+     * @param amount   金额
+     * @throws SQLException 数据库查询错误
+     */
+    public static void updateWallet(String userName, double amount) throws SQLException {
+
+    }
+
+    /**
+     * 更新商家评分
+     *
+     * @param userName  用户名
+     * @param ownerName 商家名
+     * @param rating    评分
+     * @param comment   评论
+     * @throws SQLException 数据库查询错误
+     */
+    public static void updateOwnerRating(String userName, String ownerName, double rating, String comment) throws SQLException {
+
+    }
+
+    /**
+     * 更新商家评分
+     *
+     * @param ownerName 商家名
+     * @return 商家评分
+     * @throws SQLException 数据库查询错误
+     */
+    public static double getOwnerRating(String ownerName) throws SQLException {
+        return 0;
+    }
+
+    /**
+     * 获取菜品列表
+     *
+     * @param ownerName 商家名
+     * @return 菜品列表
+     * @throws SQLException 数据库查询错误
+     */
+    public static List<Dish> getDishList(String ownerName) throws SQLException {
+        return null;
+    }
+
+    /**
+     * 获取订单列表
+     *
+     * @param ownerName 商家名
+     * @return 订单列表
+     * @throws SQLException 数据库查询错误
+     */
+    public static List<Order> getOwnerOrderList(String ownerName) throws SQLException {
+        return null;
+    }
+
+    /**
+     * 商家添加菜品
+     *
+     * @param ownerName 商家名
+     * @param dish      菜品
+     * @throws SQLException 数据库查询错误
+     */
+    public static void addDish(String ownerName, Dish dish) throws SQLException {
+
+    }
+
+    /**
+     * 商家修改菜品
+     *
+     * @param ownerName 商家名
+     * @param dish      菜品
+     * @throws SQLException 数据库查询错误
+     */
+    public static void changeDish(String ownerName, Dish dish) throws SQLException {
+
+    }
+
+    /**
+     * 商家删除菜品
+     *
+     * @param ownerName 商家名
+     * @param dish      菜品
+     * @throws SQLException 数据库查询错误
+     */
+    public static void deleteDish(String ownerName, Dish dish) throws SQLException {
+
     }
 
     public static void test_select() throws SQLException {
---
+++ src/database/sql.sql
@@ -0,0 +1,59 @@
+create table customer
+(
+    id     int primary key auto_increment,
+    name   varchar(20) not null unique,
+    wallet double default 0.0
+);
+
+create table owner
+(
+    id          int primary key auto_increment,
+    name        varchar(20) not null unique,
+    description varchar(255) default null,
+    rating      double       default 5.0
+);
+
+create table dish
+(
+    id           int primary key auto_increment,
+    name         varchar(20) not null unique,
+    introduction varchar(255) default null,
+    price        double,
+    sales        int          default 0,
+    remaining    int          default 0,
+    category     int,
+    picture      varchar(255),
+    owner_id     int         not null,
+    foreign key (owner_id) references owner (id)
+);
+
+create table orders
+(
+    id          int primary key auto_increment,
+    customer_id int not null,
+    owner_id    int not null,
+    total       double  default 0.0,
+    finished    boolean default false,
+    foreign key (owner_id) references owner (id) on delete cascade,
+    foreign key (customer_id) references customer (id) on delete cascade
+);
+
+create table order_dish
+(
+    orders_id int,
+    dish_id   int,
+    count     int,
+    foreign key (orders_id) references orders (id) on delete cascade,
+    foreign key (dish_id) references dish (id) on delete cascade
+);
+
+create table comments
+(
+    id          int primary key auto_increment,
+    customer_id int          not null,
+    owner_id    int          not null,
+    content     varchar(255) not null,
+    rating      double default 5.0,
+    foreign key (customer_id) references customer (id) on delete cascade,
+    foreign key (owner_id) references owner (id) on delete cascade
+);

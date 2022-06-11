cab3f1e247f529507a9f4fc630c1d089c95a6b01
MaxLong2001 <20373785@buaa.edu.cn>
2022-06-11 16:28:21 +0800

    lyz: 数据库

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import database.Database;
 import frontend.Frontend;
 
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         System.out.println("Hello World!");
         new Frontend();
+
+        Database.init();
+        Database.test_select();
     }
 }
--- src/database/Database.java
+++ src/database/Database.java
@@ -1,8 +1,6 @@
 package database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Database {
     /**
@@ -11,16 +9,26 @@ public class Database {
      *
      * 或可考虑在服务器上搭建数据库
      */
-    static final String URL = "jdbc:mysql://localhost:3306/order";
-    static final String USER = "root";
-    static final String PASS = "Longyizhou2001";
+    static final String URL = "jdbc:mysql://rm-bp10c5dfstb3h6q96mo.mysql.rds.aliyuncs.com/order_app";
+    static final String USER = "user";
+    static final String PASS = "User_2021";
     static Connection conn;
+    static Statement stmt;
 
     public static void init() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
+        Class.forName("com.mysql.cj.jdbc.Driver");
 
         System.out.println("Connecting to database...");
         conn = DriverManager.getConnection(URL, USER, PASS);
         System.out.println("Connected to database.");
+    }
+
+    public static void test_select() throws SQLException {
+        stmt = conn.createStatement();
+        String sql = "select id from test_table";
+        ResultSet rs = stmt.executeQuery(sql);
+        while (rs.next()) {
+            System.out.println(rs.getInt("id"));
+        }
     }
 }

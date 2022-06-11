69ae1ab6cb746525054c125b8e0c999c1386f3c3
MaxLong2001 <20373785@buaa.edu.cn>
2022-06-11 14:17:30 +0800

    lyz: 初始化本地数据库

--- src/database/Database.java
+++ src/database/Database.java
@@ -1,4 +1,19 @@
 package database;
 
+import java.sql.*;
+
 public class Database {
+    public static void init() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+
+        /**
+         * 本地数据库连接信息
+         */
+        final String url = "jdbc:mysql://localhost:3306/order";
+        final String user = "root";
+        final String pass = "Longyizhou2001";
+
+        System.out.println("Connecting to database...");
+        Connection conn = DriverManager.getConnection(url, user, pass);
+    }
 }

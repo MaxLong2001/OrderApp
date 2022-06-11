cbc6156d23f9932b2dc6385d191b792cb49b4088
evoLonation <1838940019@qq.com>
2022-06-11 15:00:30 +0800

    zzy: 构建一个窗口并成功打开

--- src/frontend/Frontend.java
+++ src/frontend/Frontend.java
@@ -1,4 +1,12 @@
 package frontend;
 
-public class Frontend {
+import javax.swing.*;
+
+public class Frontend extends JFrame{
+    public Frontend(){
+        setSize(1024, 768);
+        setVisible(true);
+        setTitle("购餐系统");
+    }
+
 }

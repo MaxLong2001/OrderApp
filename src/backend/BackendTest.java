af541d1d854b46dc21b8cf7b51650da27bd719e3
Xingru Jiang <boxuan10258@qq.com>
2022-06-19 02:06:39 +0800

    jxr：测试了商家默认推荐算法

--- src/backend/BackendTest.java
+++ src/backend/BackendTest.java
@@ -1,7 +1,7 @@
 package backend;
 
 import backend.AppException.AppException;
-import backend.Search.SearchAll;
+import backend.Recommend.ForCustomer;
 import database.Database;
 
 import java.sql.SQLException;
@@ -12,7 +12,7 @@ public class BackendTest {
     public static void print(List<Owner> owners){
 
         for(Owner owner: owners){
-            System.out.println(owner.name);
+            System.out.println(owner.name + " " + owner.rating + " " + owner.visit);
         }
     }
 
@@ -20,13 +20,23 @@ public class BackendTest {
 
         Database.init();
 
-        List<Owner> owners = null;
+        /*List<Owner> owners = null;
         try{
             owners = SearchAll.MySearch("new", Database.getAllOwner());
         }catch (AppException e){
             System.out.println(e);
         }
         assert owners != null;
+        print(owners);*/
+
+        List<Owner> owners = null;
+        try {
+            owners = ForCustomer.OwnerRecommend(Database.getAllOwner());
+        }catch (AppException e){
+            System.out.println(e);
+        }
+
+        assert false;
         print(owners);
     }
 }

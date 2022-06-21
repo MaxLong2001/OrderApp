8fb7cd34d97ae60c20eea200b2f84a72b6fd84b9
Xingru Jiang <boxuan10258@qq.com>
2022-06-21 08:31:19 +0800

    jxr:finished

--- src/backend/Recommend/ForCustomer.java
+++ src/backend/Recommend/ForCustomer.java
@@ -44,49 +44,35 @@ public class ForCustomer{
      * @throws backend.AppException.AppException 通用异常
      */
     public static List<Owner> OwnerRecommend(List<Owner> owners) throws AppException {
-
         // 推荐全数列表
         List<ForSort> forSorts = new ArrayList<>();
-
         // 遍历商家表获取权重表
         for (Owner owner : owners) {
-
-            // 临时商家变量
-            // 临时评分变量
             double temp_rating;
-
-            // 临时权重变量
             double rank;
-
             // 获取每个商家的评分
             try {
                 temp_rating = Database.getOwnerRating(owner.getName());
             } catch (SQLException e) {
                 throw new AppException("数据库异常！！");
             }
-
             // 计算权重
             rank = temp_rating * define + owner.visit * (100 - define);
-
             // 建立并插入“全数”
             ForSort forSort = new ForSort();
             forSort.owner = owner;
             forSort.rank = rank;
             forSorts.add(forSort);
         }
-
         // 对"全数列表”进行从大到小排序
         forSorts.sort(((o1, o2) -> (int) (o2.rank - o1.rank)));
-
         // 清空以前的商家表
         owners.clear();
-
         // 将“全数”表，赋给当前商家表
         for (ForSort forSort : forSorts) {
             // 临时ForSort变量
             owners.add(forSort.owner);
         }
-
         return owners;
     }
 }
